82ec12cd6bad710b20357e585165502667b69c1f
Yuval Adam <yuv.adm@gmail.com>
2012-10-14 15:57:42 +0200

    Use redis as cache backend

--- tileserver.py
+++ tileserver.py
@@ -4,22 +4,9 @@ import json
 
 import TileStache
 
-if 'AWS_ACCESS_KEY_ID' in os.environ and \
-   'AWS_SECRET_ACCESS_KEY' in os.environ:
-        cache = {
-            "name": "S3",
-            "bucket": "telostats-tiles",
-            "access": os.environ['AWS_ACCESS_KEY_ID'],
-            "secret": os.environ['AWS_SECRET_ACCESS_KEY']
-        }
-else:
-    cache = {"name": "Test"}
-
 cache = {
-  'name': 'Memcache',
-  'servers': [os.environ.get('MEMCACHIER_SERVERS')],
-  'username': os.environ.get('MEMCACHIER_USERNAME'),
-  'password': os.environ.get('MEMCACHIER_PASSWORD'),
+    "name": "Redis",
+    "url": os.getenv('REDISTOGO_URL')
 }
 
 config_dict = {
